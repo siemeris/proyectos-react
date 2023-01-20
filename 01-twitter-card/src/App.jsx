2fbdbef3824186b75efe5c514323f121731c69b4
siemeris <siemeris@gmail.com>
2023-01-20 09:51:24 +0000

    twitter card app component

---
+++ 01-twitter-card/src/App.jsx
@@ -0,0 +1,19 @@
+export function App() {
+  return (
+  <div>
+  <h1>Twitter Card</h1>
+
+  <article>
+    <header>
+      <img src="https://unavatar.io/gfty" alt="El avatar de gfty" />
+      <div>
+        <strong>Nombre inventado</strong>
+        <span>@gfty</span>
+      </div>
+    </header>
+    <aside>
+      <button>Seguir</button>
+    </aside>
+  </article>
+  </div>);
+}
