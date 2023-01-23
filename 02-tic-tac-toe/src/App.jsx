4b702085cda09e1ba0f8c31033838dc65dae8d72
siemeris <siemeris@gmail.com>
2023-01-23 08:09:19 +0000

    board

--- 02-tic-tac-toe/src/App.jsx
+++ 02-tic-tac-toe/src/App.jsx
@@ -7,24 +7,30 @@ const TURNS = {
   O: 'o'
 }
 
-const Square = ({ value, onClick }) => {}
+const Square = ({ value, onClick }) => { }
 
 function App() {
 
-  const [board, setBoard] = useState (Array(9).fill(null))
+  const [board, setBoard] = useState(Array(9).fill(null))
 
   return (
-  <main className="board">
-  <h1>Tic tac toe</h1>
-  <section>
-    {
-      board.map((square, index) => {
-        return <button key={index}>{square}</button>
-      })
-    }
-    
-  </section>
-  </main>)
+    <main className="board">
+      <h1>Tic tac toe</h1>
+      <section className='game'>
+        {
+          board.map((_, index) => {
+            return (
+            <div className='cell' key={index}>
+            <span className='cell_content'>
+             {index} 
+            </span>
+            </div>
+              )
+          })
+        }
+
+      </section>
+    </main>)
 }
 
 export default App
