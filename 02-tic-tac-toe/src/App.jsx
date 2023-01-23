79fa40521c2462d0243510a82b56203e7c39c390
siemeris <siemeris@gmail.com>
2023-01-23 08:42:18 +0000

    visualizando turno

--- 02-tic-tac-toe/src/App.jsx
+++ 02-tic-tac-toe/src/App.jsx
@@ -7,11 +7,19 @@ const TURNS = {
   O: 'o'
 }
 
-const Square = ({ value, onClick }) => { }
+const Square = ({ children, isSelected, updateBoard, index }) => {
+  const className = isSelected ? 'square is-selected' : 'square'
+  return (
+    <div className={className}>
+      {children}
+    </div>
+  )
+ }
 
 function App() {
 
   const [board, setBoard] = useState(Array(9).fill(null))
+  const [turn, setTurn] = useState(TURNS.X)
 
   return (
     <main className="board">
@@ -20,15 +28,15 @@ function App() {
         {
           board.map((_, index) => {
             return (
-            <div className='cell' key={index}>
-            <span className='cell_content'>
-             {index} 
-            </span>
-            </div>
+              <Square key={index}> {board[index]} </Square>
               )
           })
         }
 
+      </section>
+      <section className="turn">
+        <Square isSelected={turn === TURNS.X } >{TURNS.X}</Square>
+        <Square isSelected={turn === TURNS.O }>{TURNS.O}</Square>
       </section>
     </main>)
 }
