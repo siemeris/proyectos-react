0be6cfe92b6f14a9a6520eb72093884c4f518459
siemeris <siemeris@gmail.com>
2023-01-24 08:44:35 +0000

    EventListener

--- 03-mouse-follower/src/App.jsx
+++ 03-mouse-follower/src/App.jsx
@@ -1,6 +1,44 @@
+import { useEffect, useState } from 'react'
 
 function App () {
-  return (<h3>Proyecto 3</h3>)
+  const [enabled, setEnabled] = useState(false)
+  const [position, setPosition] = useState({ x: 0, y: 0 })
+
+  useEffect(() => {
+    const handleMove = (event) => {
+      const { clientX, clientY } = event
+      console.log('handleMove', { clientX, clientY })
+      setPosition({ x: clientX, y: clientY })
+    }
+    if (enabled) {
+      window.addEventListener('pointermove', handleMove)
+    }
+    return () => {
+      // Este return se ejecuta cuando se desmonta el componente (App) o cuando se actualiza el estado enabled
+      window.removeEventListener('pointermove', handleMove)
+    }
+  }, [enabled])
+
+  return (
+    <main>
+      <div style={{
+        position: 'absolute',
+        backgroundColor: 'rgba(0, 0, 0, 0.5)',
+        border: '1px solid #fff',
+        borderRadius: '50%',
+        opacity: 0.8,
+        pointerEvents: 'none',
+        left: -25,
+        top: -25,
+        width: 50,
+        height: 50,
+        transform: `translate(${position.x}px, ${position.y}px)`
+      }}
+      />
+      <h3>Proyecto 3</h3>
+      <button onClick={() => { setEnabled(!enabled) }}>{enabled ? 'Desactivar' : 'Activar'} seguir puntero</button>
+    </main>
+  )
 }
 
 export default App
