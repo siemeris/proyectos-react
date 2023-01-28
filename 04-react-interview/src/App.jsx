3dce8000e45c565f6d00bf4423216cd498d0b096
siemeris <siemeris@gmail.com>
2023-01-28 16:15:48 +0000

    App.css

--- 04-react-interview/src/App.jsx
+++ 04-react-interview/src/App.jsx
@@ -1,45 +1,50 @@
 import React, { useEffect, useState } from 'react'
+import './App.css'
 
 export function App () {
-  const [fact, setFact] = useState([])
-  const [firstWord, setFirstWord] = useState('')
-  const [imageUrl, setImageUrl] = useState('')
+  const [fact, setFact] = useState()
+  const [imageUrl, setImageUrl] = useState()
 
   const CAT_ENDPOINT_RANDOM_FACT = 'https://catfact.ninja/fact'
   //   const URL_IMAGE = `https://cataas.com/cat/says/${firstWord}`
   // const CAT_ENDPOINT_IMAGE_URL = `https://cataas.com/cat/says/${threeFirstWord}?size=50&color=red&json=true`
-  
+  const CAT_PREFIX_IMAGE_URL = 'https://cataas.com'
 
+  // Para recuperar l cita al cargar la página
   useEffect(() => {
     fetch(CAT_ENDPOINT_RANDOM_FACT)
       .then((res) => res.json())
-      .then((data) => { 
+      .then((data) => {
         const { fact } = data
         setFact(fact)
-        const firstWord = fact.split(' ')[0]
-        setFirstWord(firstWord)
-        // Si nos queremos quedar con las tres primeras palabras
-        // podemos usar 
-        // fact.split(' ' , 3).join(' ')
       })
+  }, [])
 
+  // Para recuperar la imagen cada vez que tenemos una cita nueva
+  useEffect(() => {
+    if (!fact) return
+    const firstWord = fact.split(' ')[0]
+    console.log(firstWord)
+    // Si nos queremos quedar con las tres primeras palabras
+    // podemos usar
+    // fact.split(' ' , 3).join(' ')
     fetch(`https://cataas.com/cat/says/${firstWord}?size=50&color=red&json=true`)
-      .then((res) => res.json())
-      .then((data) => {
-        console.log(data)
-        const { url } = data
+      .then(res => res.json())
+      .then(response => {
+        console.log(response)
+        const { url } = response
         console.log(url)
         setImageUrl(url)
       })
-  }, [])
+  }, [fact])
 
   return (
     <main>
       <h1>Random cat fact</h1>
+      {/* <section> */}
       {fact && <p>{fact}</p>}
-      {/* <img src={`https://cataas.com/cat/says/${firstWord}`} alt={fact} /> */}
-      {imageUrl && <img src={`https://cataas.com${imageUrl}`} alt={`Image extracted using the firts word for ${fact}`}></img>}
-
+      {imageUrl && <img src={`${CAT_PREFIX_IMAGE_URL}${imageUrl}`} alt={`Image extracted using the firts word for ${fact}`} />}
+      {/* </section> */}
     </main>
 
   )
