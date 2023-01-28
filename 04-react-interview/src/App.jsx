ec16b7735c8bf2dbc6130e519b348ed691e7f867
siemeris <siemeris@gmail.com>
2023-01-28 11:22:07 +0000

    second fetch to ge the image

--- 04-react-interview/src/App.jsx
+++ 04-react-interview/src/App.jsx
@@ -3,23 +3,42 @@ import React, { useEffect, useState } from 'react'
 export function App () {
   const [fact, setFact] = useState([])
   const [firstWord, setFirstWord] = useState('')
+  const [imageUrl, setImageUrl] = useState('')
 
-  const URL = 'https://catfact.ninja/fact'
+  const CAT_ENDPOINT_RANDOM_FACT = 'https://catfact.ninja/fact'
   //   const URL_IMAGE = `https://cataas.com/cat/says/${firstWord}`
+  // const CAT_ENDPOINT_IMAGE_URL = `https://cataas.com/cat/says/${threeFirstWord}?size=50&color=red&json=true`
+  
 
   useEffect(() => {
-    fetch(URL).then((res) => res.json()).then((data) => setFact(data.fact))
-    console.log(fact)
+    fetch(CAT_ENDPOINT_RANDOM_FACT)
+      .then((res) => res.json())
+      .then((data) => { 
+        const { fact } = data
+        setFact(fact)
+        const firstWord = fact.split(' ')[0]
+        setFirstWord(firstWord)
+        // Si nos queremos quedar con las tres primeras palabras
+        // podemos usar 
+        // fact.split(' ' , 3).join(' ')
+      })
 
-    // fetch(`URL_IMAGE${fact}`).then((res) => res.json()).then((data) => setImg(data.fact))
-    // fact && setFirstWord(fact.split(' ')[0])
-    // setFirstWord(data.fact.split(' ')[0])
+    fetch(`https://cataas.com/cat/says/${firstWord}?size=50&color=red&json=true`)
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data)
+        const { url } = data
+        console.log(url)
+        setImageUrl(url)
+      })
   }, [])
 
   return (
     <main>
-      <section>{fact}</section>
-      <img src={`https://cataas.com/cat/says/${firstWord}`} alt={fact} />
+      <h1>Random cat fact</h1>
+      {fact && <p>{fact}</p>}
+      {/* <img src={`https://cataas.com/cat/says/${firstWord}`} alt={fact} /> */}
+      {imageUrl && <img src={`https://cataas.com${imageUrl}`} alt={`Image extracted using the firts word for ${fact}`}></img>}
 
     </main>
 
