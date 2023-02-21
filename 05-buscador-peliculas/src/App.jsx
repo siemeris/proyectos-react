b40073ecafa36d8e47a460f38b706e368dbcf42a
siemeris <siemeris@gmail.com>
2023-02-21 09:46:15 +0000

    refactorizado y separando en componentes

--- 05-buscador-peliculas/src/App.jsx
+++ 05-buscador-peliculas/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import './App.css'
 import responseMovies from './mocks/with-results.json'
 import withoutResults from './mocks/no-results.json'
+import {Movies} from './components/Movies'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -19,23 +20,9 @@ function App() {
       </form>
       </header>
       <main>
-        {
-          hasMovies ?
-          (
-            <ul>
-              {
-                movies.map(movie => (
-                  <li key={movie.imdbID}>
-                    <h3>{movie.Title}</h3>
-                    <p>{movie.Year}</p>
-                    <img src={movie.Poster} alt={movie.Title}></img>
-                    </li>
-                ))
-              }
-            </ul>
-          ) :
-          <p>No se encontraron películas para esta búsqueda</p>
-        } 
+
+          <Movies movies={movies} />
+         
       </main>
     </div>
   )
---
+++ 05-buscador-peliculas/src/components/Movies.jsx
@@ -0,0 +1,55 @@
+import React from 'react'
+
+function ListOfMovies ({ movies }) {
+    return (
+      <ul className='movies'>
+        {
+          movies.map(movie => (
+            <li className='movie' key={movie.imdbID}>
+              <h3>{movie.Title}</h3>
+              <p>{movie.Year}</p>
+              <img src={movie.Poster} alt={movie.title} />
+            </li>
+          ))
+        }
+      </ul>
+    )
+  }
+  
+  function NoMoviesResults () {
+    return (
+      <p>No se encontraron películas para esta búsqueda</p>
+    )
+  }
+  
+  export function Movies ({ movies }) {
+    const hasMovies = movies?.length > 0
+  
+    return (
+      hasMovies
+        ? <ListOfMovies movies={movies} />
+        : <NoMoviesResults />
+    )
+  }
+
+
+// const Movies = () => {
+//     const movies = responseMovies.Search
+//   return (
+//     <div>
+//         <ul>
+//               {
+//                 movies.map(movie => (
+//                   <li key={movie.imdbID}>
+//                     <h3>{movie.Title}</h3>
+//                     <p>{movie.Year}</p>
+//                     <img src={movie.Poster} alt={movie.Title}></img>
+//                     </li>
+//                 ))
+//               }
+//             </ul>
+//     </div>
+//   )
+// }
+
+// export default Movies
