7abf264740b51c30fb0ec60e47bf0f33a6b9870b
siemeris <siemeris@gmail.com>
2023-02-22 09:39:16 +0000

    separando lógica de validación del input

--- 05-buscador-peliculas/src/App.jsx
+++ 05-buscador-peliculas/src/App.jsx
@@ -1,36 +1,88 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 // import responseMovies from './mocks/with-results.json'
 // import withoutResults from './mocks/no-results.json'
 import { useMovies } from './hooks/useMovies'
 import { Movies } from './components/Movies'
 
+function useSearch () {
+
+  const [search, updateSearch] = useState('')
+  const [error, setError] = useState(null)
+
+  useEffect(() => {
+    // if (isFirstInput.current) {
+    //   isFirstInput.current = search === ''
+    //   return
+    // }
+
+    if (search === '') {
+      setError('No se puede buscar una película vacía')
+      return
+    }
+
+    if (search.match(/^\d+$/)) {
+      setError('No se puede buscar una película con un número')
+      return
+    }
+
+    if (search.length < 3) {
+      setError('La búsqueda debe tener al menos 3 caracteres')
+      return
+    }
+    setError(null)
+  }, [search])
+
+return {search, updateSearch, error}
+
+}
+
+
 function App() {
 
-  const{ movies} = useMovies()
+  const { movies } = useMovies()
+  // const inputRef = useRef()
+  const {search, updateSearch, error} = useSearch()
 
-  // const movies = responseMovies.Search
-  // // Mapeamos los datos de la API como buena práctica, por si en un futuro estos datos son renombrados
-  // const mappedMovies = movies?.map(movie =>({
-  //   id: movie.imdbID,
-  //   title: movie.Title,
-  //   year: movie.Year,
-  //   img: movie.Poster
-  // }) )
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    // const value = inputRef.current.value
+
+    //Para obtener todos los valores del formulario en un objeto
+    // const fields = Object.fromEntries(new window.FormData(event.target))
+    // console.log(fields)
+
+    //En nuestro caso os interesa query
+    // const {query} = Object.fromEntries(new window.FormData(event.target))
+    // console.log(query)
+  }
+
+  const handleChange = (event) => {
+    updateSearch(event.target.value)
+    console.log(search)
+  }
 
   return (
     <div className='page'>
       <header>
-      <h1>Buscador de Películas</h1>
-      <form className='form'>
-        <input type="text" placeholder="Avengers, Star Wars, Matrix"></input>
-        <button type="submit">Buscar</button>
-      </form>
+        <h1>Buscador de Películas</h1>
+        <form className='form' onSubmit={handleSubmit}>
+          {/* <input name="query2" type="text" placeholder="Avengers, Star Wars, Matrix"></input> */}
+          <input style={{
+              border: '1px solid transparent',
+              borderColor: error ? 'red' : 'transparent'
+            }} onChange={handleChange} value={search} name="query" type="text" placeholder="Avengers, Star Wars, Matrix"></input>
+          <button type="submit">Buscar</button>
+        </form>
+
+        {error && <p style={{ color: 'red' }}>{error}</p>}
+
       </header>
       <main>
 
-          <Movies movies={movies} />
-         
+        <Movies movies={movies} />
+
       </main>
     </div>
   )
