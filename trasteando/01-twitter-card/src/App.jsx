f69d5c103de31a22ac5d542703130467ef474967
siemeris <siemeris@gmail.com>
2023-03-08 13:12:06 +0000

    isFollowing state

--- trasteando/01-twitter-card/src/App.jsx
+++ trasteando/01-twitter-card/src/App.jsx
@@ -10,29 +10,37 @@ function App() {
     {
       name: 'Jose',
       username: 'joselete',
-      avatar: 'https://unavatar.io/joselete'
+      avatar: 'https://unavatar.io/joselete',
+      isFollowing: false
+    
     },
     {
       name: 'Manolo',
       username: 'manolo',
-      avatar: 'https://unavatar.io/manolo'
+      avatar: 'https://unavatar.io/manolo',
+      isFollowing: false
     },
     {
       name: 'Paco',
       username: 'paco',
-      avatar: 'https://unavatar.io/paco'
+      avatar: 'https://unavatar.io/paco',
+      isFollowing: true
     },
     {
       name: 'Pepe',
       username: 'pepe',
-      avatar: 'https://unavatar.io/pepe'
+      avatar: 'https://unavatar.io/pepe',
+      isFollowing: true
     }]
 
+    const addAt = (username) => {
+      return `@${username}`
+      }
 
   return (
     <div className="App">
-      {users.map((users)=>
-      (<TwitterCard users={users}/>))}
+      {users.map(({name, username, isFollowing})=>
+      (<TwitterCard name={name} username={username} initialIsFollowing={isFollowing} addAt={addAt}/>))}
     </div>
   )
 }
--- trasteando/01-twitter-card/src/components/TwitterCard.jsx
+++ trasteando/01-twitter-card/src/components/TwitterCard.jsx
@@ -1,23 +1,21 @@
 import React, { useState } from 'react'
 
-const TwitterCard = ({users}) => {
+const TwitterCard = ({name, username, initialIsFollowing, addAt}) => {
 
-    const [isFollowing, setIsFollowing] = useState(false)
 
-    const {name, username, avatar} = users
-
-    const addAt = (username) => {
-      return `@${username}`
-      }
+  
+    const [isFollowing, setIsFollowing] = useState(initialIsFollowing)
+    
+    
 
     return (
         <div className="card">
-        <img src="https://unavatar.io/joselete" alt=""></img>
+        <img src={`https://unavatar.io/${username}`} alt=""></img>
         <div className="card-info">
           <strong> {name}</strong>
           <span> {addAt(username)}</span>
         </div>
-        <button className="card-button" onClick={()=>{setIsFollowing(!isFollowing)}}> 
+        <button className={isFollowing ? "card-button is-following" : "card-button"} onClick={()=>{setIsFollowing(!isFollowing)}}> 
         {isFollowing ? "Siguiendo" : "Seguir"}</button>
       </div>
     )
