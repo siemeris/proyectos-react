f2ef46047261dfd4e176d0f88607773666c3f814
siemeris <siemeris@gmail.com>
2023-03-09 07:54:17 +0000

    button

--- trasteando/04-fetch/src/App.jsx
+++ trasteando/04-fetch/src/App.jsx
@@ -9,21 +9,27 @@ function App() {
   // const ENDPOINT_IMAGE = `https://cataas.com/cat/says/${primeraPalabra}?json=true`
   const [imageUrl, setImageUrl] = useState()
 
+  const getRandomFact = () => {
+    fetch(ENDPOINT_HECHO)
+    .then((res) => res.json()) //La respuesta la transformamos a JSON
+    .then((response) => {
+      const {fact} = response
+      setData(fact)
+      const primeraPalabra = fact.split(' ')[0]
+ // `https://cataas.com/cat/says/${primeraPalabra}?json=true`
+
+      fetch(`https://cataas.com/cat/says/${primeraPalabra}?json=true`)
+    .then((res) => res.json())
+    .then(response => setImageUrl(response.url))
+    })
+  }
+
 
   useEffect(() => {
-    fetch(ENDPOINT_HECHO)
-      .then((res) => res.json()) //La respuesta la transformamos a JSON
-      .then((response) => {
-        const {fact} = response
-        setData(fact)
-        const primeraPalabra = fact.split(' ')[0]
-   // `https://cataas.com/cat/says/${primeraPalabra}?json=true`
-
-        fetch(`https://cataas.com/cat/says/${primeraPalabra}?json=true`)
-      .then((res) => res.json())
-      .then(response => setImageUrl(response.url))
-      })
+    getRandomFact()
   }, [])
+
+
 
 
   return (
@@ -36,6 +42,9 @@ function App() {
       </div>
       {imageUrl && <img src={`https://cataas.com/${imageUrl}`}></img>}
       </section>
+      <div className="button-changefact">
+      <button onClick={getRandomFact}>Get Random Fact</button>
+      </div>
     </div>
   )
 }
