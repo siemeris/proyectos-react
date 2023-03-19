8f7783a8969417d09e2ff60e38db6ff524b4acba
siemeris <siemeris@gmail.com>
2023-03-19 18:04:48 +0000

    useMovies

--- 05-buscador-peliculas/src/hooks/useMovies.js
+++ 05-buscador-peliculas/src/hooks/useMovies.js
@@ -2,6 +2,8 @@ import withResults from '../mocks/with-results.json'
 import withoutResults from '../mocks/no-results.json'
 import { useState } from 'react'
 
+const API_KEY = '9b0e8ff2'
+
 export function useMovies({search}) {
     const [responseMovies, setResponseMovies] = useState([])
 
@@ -15,9 +17,11 @@ export function useMovies({search}) {
     }) )
 
     const getMovies = ()=>{
+        if(search === '') return null // Si es vacío, no hacemos el fetching de datos
+        
         if(search){
           // setResponseMovies(withResults)
-          fetch(`https://www.omdbapi.com/?apikey=9b0e8ff2&s=${search}`)
+          fetch(`https://www.omdbapi.com/?apikey=${API_KEY}&s=${search}`)
             .then(res => res.json())
             .then(json => setResponseMovies(json))
         }
