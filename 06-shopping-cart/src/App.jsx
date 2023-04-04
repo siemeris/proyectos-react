3fb201038bea30d234ad8454b3f6447e9ed48119
siemeris <siemeris@gmail.com>
2023-04-04 08:03:49 +0000

    Showing Poducts

--- 06-shopping-cart/src/App.jsx
+++ 06-shopping-cart/src/App.jsx
@@ -1,8 +1,12 @@
+import { products } from './mocks/products.json'
+import { Products } from './components/Products'
+
 function App() {
 
 
   return (
-      <h1>Shopping Cart</h1>
+      <Products products={products} />
+
   )
 }
 
