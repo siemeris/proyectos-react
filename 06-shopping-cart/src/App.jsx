35aca3e1c514658b213e73ca4e229fa39dadda7e
siemeris <siemeris@gmail.com>
2023-04-04 08:42:23 +0000

    filter functionality

--- 06-shopping-cart/src/App.jsx
+++ 06-shopping-cart/src/App.jsx
@@ -1,11 +1,34 @@
-import { products } from './mocks/products.json'
+import { products as initialProducts } from './mocks/products.json'
 import { Products } from './components/Products'
+import { useState } from 'react'
 
 function App() {
 
+  const[products]=useState(initialProducts)
+  const[filters, setFilters]=useState(
+    {
+      category:'all',
+      minPrice:0
+    }
+  )
+
+  const filterProducts = (products) =>{
+    return products.filter(product=>{
+      return (
+        product.price >= filters.minPrice && 
+        (
+          filters.category == 'all' ||
+          product.category == filters.category
+
+        )
+      )
+    })
+  } 
+
+  const filteredProducts = filterProducts(products)
 
   return (
-      <Products products={products} />
+      <Products products={filteredProducts} />
 
   )
 }
