5853ca63955ba2724eb4bf5b91a90cc6f0d4e9a4
siemeris <siemeris@gmail.com>
2023-04-05 07:10:02 +0000

    min price value

--- 06-shopping-cart/src/components/Filters.jsx
+++ 06-shopping-cart/src/components/Filters.jsx
@@ -1,14 +1,20 @@
+import { useState } from 'react'
 import './Filters.css'
 
 export function Filters(){
 
-    
+   const [minPrice, setMinPrice] = useState(0) 
+
+   const handleChangeMinPrice = (event) => {
+        setMinPrice(event.target.value)
+   }
 
     return(
         <section className='filters'>
             <div>
                 <label htmlFor="price">Price</label>
-                <input type="range" id="price" min="0" max="1000"></input>
+                <input type="range" id="price" min="0" max="1000" onChange={handleChangeMinPrice}></input>
+                <span>$ {minPrice} </span>
             </div>
             <div>
                 <label htmlFor="category">Category</label>
